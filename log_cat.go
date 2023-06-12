b9524c8c4583cb5ea3934db3c7bcc9cf625f3df4
Roberto Hernandez <roberto.hernandez@uvltd.tech>
2023-06-12 17:37:02 -0600

    Added 2 more datastore categories

--- log_cat.go
+++ log_cat.go
@@ -14,6 +14,12 @@ var (
 	// LogCatHealth usage: health check message
 	LogCatHealth = LogCat{Code: "HTH001", Type: "health_check"}
 
+	// LogCatDatabase usage: datastore connection message
+	LogCatDatabaseConnection = LogCat{Code: "DTA001", Type: "datastore_connection"}
+
+	// LogCatDatastoreClose usage: close datastore connection message
+	LogCatDatastoreClose = LogCat{Code: "DTA002", Type: "datastore_close"}
+
 	// LogCatDatabase usage: datastore interaction
 	// Logs, e.g. query exec or record read errors
 	LogCatDatabase = LogCat{Code: "DTA003", Type: "datastore_interaction"}
